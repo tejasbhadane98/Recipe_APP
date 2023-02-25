7a8205567050aa9de004e82e7776acecafa3543e
tejasbhadane98 <119942748+tejasbhadane98@users.noreply.github.com>
2023-02-25 17:11:21 +0530

    Updated File

--- recipe_app/index.js
+++ recipe_app/index.js
@@ -19,6 +19,13 @@ app.use(user)
 const recipe = require("./routes/recipe")
 app.use(recipe)
 
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server Running at the port 5000");
 });
